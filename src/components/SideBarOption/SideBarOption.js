2b9fca1d98744a41be387b9f12f3a15d241b0497
MaximilienRabourdin <maximilien.rabourdin@gmail.com>
2021-11-22 22:06:36 +0100

    sidebar construction

--- src/components/SideBarOption/SideBarOption.js
+++ src/components/SideBarOption/SideBarOption.js
@@ -1,38 +1,38 @@
-import React from 'react'
-import SideBarOptionStyled from './SideBarOptionStyted';
+// import React from 'react'
+// import SideBarOptionStyled from './SideBarOptionStyted';
 
-function SideBarOption({Icon, title, addChanelOption}) {
-    return (
+// function SideBarOption({Icon, title, addChanelOption}) {
+//     return (
 
-        // const addChannel = (e) => {
+//         // const addChannel = (e) => {
 
-        // }
+//         // }
 
         
-        // const selectChannel = (e) => {
+//         // const selectChannel = (e) => {
             
-        // }
+//         // }
 
-        <SideBarOptionStyled>
+//         <SideBarOptionStyled>
 
-        <div className="sidebar-container" onClick={addChanelOption ? addChannel : selectChannel }>
-            {Icon && <Icon fontSize="small" style={{padding: 10 }} />}
-            { Icon ? (
-                <h3>
-                    {title}
-                </h3>
-             )
-              : (
-                  <div className="sidebaroptionchannel">
-                  <span>#</span> {title}
-                  </div>
-              )
-              }
-        </div>
+//         <div className="sidebar-container" onClick={addChanelOption ? addChannel : selectChannel }>
+//             {Icon && <Icon fontSize="small" style={{padding: 10 }} />}
+//             { Icon ? (
+//                 <h3>
+//                     {title}
+//                 </h3>
+//              )
+//               : (
+//                   <div className="sidebaroptionchannel">
+//                   <span>#</span> {title}
+//                   </div>
+//               )
+//               }
+//         </div>
    
-        </SideBarOptionStyled>
+//         </SideBarOptionStyled>
    
-    );
-}
+//     );
+// }
 
-export default SideBarOption;
+// export default SideBarOption;
