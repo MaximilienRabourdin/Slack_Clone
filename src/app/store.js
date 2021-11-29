d2fb7321a6ed5b0f567f8e5eb97dd48b9ce114c2
MaximilienRabourdin <maximilien.rabourdin@gmail.com>
2021-11-29 20:02:25 +0100

    v1 slackclone with google auth

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import appSlice from '../features/appSlice';
+import appReducer from "../features/appSlice";
 
-export default configureStore ({
-    reducer: {
-        app:appSlice,
-    }
-})
+export default configureStore({
+  reducer: {
+    app: appReducer,
+  },
+});
--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,19 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
-
+// Intrégration de firebase
+import firebase from "firebase";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCx3Rf6bpmidOy1yjv6cUVxH6CHGST2KyI",
-    authDomain: "slack-clone-fa7da.firebaseapp.com",
-    projectId: "slack-clone-fa7da",
-    storageBucket: "slack-clone-fa7da.appspot.com",
-    messagingSenderId: "5325137386",
-    appId: "1:5325137386:web:a68540f80d1df2e957fe93",
-    // measurementId: "G-ZK4PFJPWYJ"
-  };
+  apiKey: "AIzaSyCx3Rf6bpmidOy1yjv6cUVxH6CHGST2KyI",
+  authDomain: "slack-clone-fa7da.firebaseapp.com",
+  projectId: "slack-clone-fa7da",
+  storageBucket: "slack-clone-fa7da.appspot.com",
+  messagingSenderId: "5325137386",
+  appId: "1:5325137386:web:a68540f80d1df2e957fe93",
+};
 
-  const firebaseApp = firebase.initializeApp(firebaseConfig)
-  const db = firebaseApp.firestore();
-  const auth = firebase.auth();
-  const provider = new firebase.auth.GoogleAuthProvider();
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
 
-  export { auth, provider, db };
+export { auth, provider, db };
